96538a53cbbc4dc95623abd2c645691750b332f2
thang.buingoc <thangbn.pysoft@gmail.com>
2022-04-15 22:50:45 +0700

    Dung UI

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,30 +1,98 @@
 export const MOCK_DATA_BOARD = {
   lanes: [
     {
-      id: "lane1",
-      title: "Planned Tasks",
-      label: "2/2",
       cards: [
         {
-          id: "Card1",
-          title: "Write Blog",
-          description: "Can AI make memes",
-          label: "30 mins",
+          description: "2 Gallons of milk at the Deli store",
+          id: "Milk",
+          label: "15 mins",
+          laneId: "PLANNED",
+          title: "Buy milk",
         },
         {
-          id: "Card2",
-          title: "Pay Rent",
-          description: "Transfer via NEFT",
+          description: "Sort out recyclable and waste as needed",
+          id: "Plan2",
+          label: "10 mins",
+          laneId: "PLANNED",
+          title: "Dispose Garbage",
+        },
+        {
+          description: "Can AI make memes?",
+          id: "Plan3",
+          label: "30 mins",
+          laneId: "PLANNED",
+          title: "Write Blog",
+        },
+        {
+          description: "Transfer to bank account",
+          id: "Plan4",
           label: "5 mins",
-          metadata: { sha: "be312a1" },
+          laneId: "PLANNED",
+          title: "Pay Rent",
         },
       ],
+      currentPage: 1,
+      disallowAddingCard: true,
+      id: "PLANNED",
+      label: "20/70",
+      style: {
+        width: 280,
+      },
+      title: "Disallowed adding card",
     },
     {
-      id: "lane2",
-      title: "Completed",
-      label: "0/0",
+      cards: [
+        {
+          description:
+            "Soap wash and polish floor. Polish windows and doors. Scrap all broken glasses",
+          id: "Wip1",
+          label: "30 mins",
+          laneId: "WIP",
+          title: "Clean House",
+        },
+      ],
+      currentPage: 1,
+      id: "WIP",
+      label: "10/20",
+      style: {
+        width: 280,
+      },
+      title: "Work In Progress",
+    },
+    {
       cards: [],
+      currentPage: 1,
+      id: "BLOCKED",
+      label: "0/0",
+      style: {
+        width: 280,
+      },
+      title: "Blocked",
+    },
+    {
+      cards: [
+        {
+          description: "Use Headspace app",
+          id: "Completed1",
+          label: "15 mins",
+          laneId: "COMPLETED",
+          title: "Practice Meditation",
+        },
+        {
+          description: "Use Spreadsheet for now",
+          id: "Completed2",
+          label: "15 mins",
+          laneId: "COMPLETED",
+          title: "Maintain Daily Journal",
+        },
+      ],
+      currentPage: 1,
+      id: "COMPLETED",
+      label: "2/5",
+      style: {
+        width: 280,
+      },
+      title: "Completed",
     },
   ],
 };
--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -1,7 +1,22 @@
+import { Button } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Board from "react-trello";
 import { MOCK_DATA_BOARD } from "../../mocks";
 
 export default function BoardView() {
-  return <Board data={MOCK_DATA_BOARD} />;
+  const navigate = useNavigate();
+  return (
+    <div className="container__wrapper">
+      <div className="list__project__header">
+        <h3>List Tasks Of Project</h3>
+      </div>
+      <div style={{ paddingLeft: 20, paddingBottom: 20 }}>
+        <Button type="primary" onClick={() => navigate("/")}>
+          Back to Projects
+        </Button>
+      </div>
+      <Board style={{ width: "100%", minHeight: 500}} data={MOCK_DATA_BOARD} draggable editable canAddLanes />
+    </div>
+  );
 }
